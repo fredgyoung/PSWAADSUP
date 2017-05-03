3e99a0f74c0433be4b6ee05cd6d0bb9d1ea96d97
fredgyoung <fredgyoung@gmail.com>
2017-05-03 15:57:23 -0700

    new file

---
+++ HashTable.py
@@ -0,0 +1,29 @@
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data - data
+        self.next = None
+
+
+class HashTable:
+
+    def __init__(self, size):
+        self.array = []
+        self.size = size
+        for _ in range(self.size):
+            self.array.append(None)
+
+
+def is_prime(num):
+    pass
+
+
+def find_next_prime(num):
+    pass
+
+
+if __name__ == '__main__':
+
+    ht = HashTable(10)
